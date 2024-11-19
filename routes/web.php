c84367dbcd477a0e88c3f1b180f454196918047c
HnZzyyz <aldairsyafie@gmail.com>
2024-11-19 16:49:14 +0700

    login-session

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('login',[AdminController::class,'index'])->name('login');
+Route::post('login',[AdminController::class,'cekLogin']);
+
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/',[AdminController::class,'dashboard'])->name('dashboard');
 });
+Route::get('logout',[AdminController::class,'logout'])->name('logout');
